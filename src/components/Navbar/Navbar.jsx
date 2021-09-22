4fd05503e4ed1aed7414681aecb64e923523cba4
BozzleMay <ledley26@msn.com>
2021-09-22 17:03:49 +0100

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton,Badge, MenuItem, Menu, Typorgraphy, Typography} from '@material-ui/core'
+import { ShoppingCart} from '@material-ui/icons'
+import {Link, useLocation} from 'react-router-dom'
+
+import useStyles from './styles';
+import logo from './../../assets/shop.jpg'
+
+const Navbar = ({totalItems}) => {
+    const classes = useStyles()
+    const location = useLocation()
+
+    
+    return (
+        <div>
+           <AppBar position='fixed' className={classes.appBar} color='inherit'>
+                    <Toolbar>
+                        <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
+                            <img src={logo} alt='Commerce.js' height="25px" className={classes.image} />
+                            Barry's Shop
+                        </Typography>
+                        <div className={classes.grow} />
+                        {location.pathname === '/' && ( 
+                        <div className={classes.button} >
+                        <IconButton component={Link} to='/cart' aria-label='show cart times' color='inherit'>
+                            <Badge badgeContent={totalItems} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                        </div> )}
+                    </Toolbar>
+           </AppBar>
+            
+        </div>
+    )
+}
+
+export default Navbar
+
---
+++ src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce('pk_test_3353538154b88e1e98e07a19d958a148e350373d37c47', true)
